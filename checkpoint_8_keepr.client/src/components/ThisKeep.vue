284e29cb330403b18baa7dc23345c78e1ee463a1
Josue Hernandez <josuehdz0@gmail.com>
2023-04-03 12:34:25 -0600

    most of thiskeep cards done

--- checkpoint_8_keepr.client/src/components/ThisKeep.vue
+++ checkpoint_8_keepr.client/src/components/ThisKeep.vue
@@ -1,7 +1,53 @@
 <template>
   <div v-if="keep">
-    {{ keep.name }}
-    test?
+
+    <div class="container-fluid">
+      <div class="row">
+        <!-- NOTE Doing weird stuff to not get rounded edges on picture in mobile -->
+        <div class="col-md-6 p-0 d-md-none photo">
+          <img :src="keep.img" :alt="keep.name" class="img-fluid ">
+          <button type="button" class="btn-close p-2 d-md-none top-right" data-bs-dismiss="modal"
+            aria-label="Close"></button>
+        </div>
+        <div class="col-md-6 p-0 d-none d-md-flex">
+          <img :src="keep.img" :alt="keep.name" class="img-fluid rounded-start">
+        </div>
+        <!-- NOTE Keep info -->
+        <div class="col-md-6 d-flex flex-column justify-content-between infosize">
+
+          <div class="row justify-content-center text-center pt-3">
+            <div class="col-3">
+              <div class="mdi mdi-eye-outline"></div>
+
+            </div>
+            <div class="col-3">
+              <div class="mdi mdi-alpha-k-circle-outline"></div>
+
+            </div>
+          </div>
+          <div class="row justify-content-center ">
+            <h2 class="text-center">
+              {{ keep.name }}
+            </h2>
+            <p>
+              {{ keep.description }}
+            </p>
+          </div>
+
+          <div class="row">
+            <b class="col-8 d-flex align-items-center">My Vaults
+              <button class="btn"> Save</button>
+            </b>
+            <div class="col-4 d-flex justify-content-end">
+              <img :src="keep.creator.picture" alt=" photo" height="50" class="rounded-circle p-2 m-0 bottom-right" />
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
 
 
   </div>
@@ -22,4 +68,22 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.photo {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+
+.infosize {
+  height: auto;
+  min-height: 100%;
+}
+
+.top-right {
+  position: absolute;
+  text-shadow: 1px 1px 15px #00000086;
+  top: 1px;
+  right: .01em;
+}
+</style>

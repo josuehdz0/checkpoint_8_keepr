44e16acc2d357fb4bf0d8f52110be8c1acf01abd
Josue Hernandez <josuehdz0@gmail.com>
2023-04-13 13:59:45 -0600

    deploying project

--- checkpoint_8_keepr.client/vite.config.js
+++ checkpoint_8_keepr.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../checkpoint_8_keepr/wwwroot',
     sourcemap: false
   },
   server: {
